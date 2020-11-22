6f127985f3a74a3ff9713d4eb555599e4b960014
root <root@kirin.chubachi.net>
2020-11-23 00:52:44 +0900

    Create README.org

--- init.el
+++ init.el
@@ -1,8 +1,7 @@
 ;;; init.el --- My init.el  -*- lexical-binding: t; -*-
+;; Copyright (C) 2020 Yoshihide Chubachi
 
-;; Copyright (C) 2020  Naoya Yamashita
-
-;; Author: Naoya Yamashita <conao3@gmail.com>
+;; Author: Yoshihide Chubachi <yoshi@chubachi.net>
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -19,146 +18,14 @@
 
 ;;; Commentary:
 
-;; My init.el.
+;;  My init.el.
 
 ;;; Code:
 
-;; this enables this running method
-;;   emacs -q -l ~/.debug.emacs.d/{{pkg}}/init.el
-(eval-and-compile
-  (when (or load-file-name byte-compile-current-file)
-    (setq user-emacs-directory
-          (expand-file-name
-           (file-name-directory
-            (or load-file-name byte-compile-current-file))))))
-
-(eval-and-compile
-  (customize-set-variable
-   'package-archives '(("org"   . "https://orgmode.org/elpa/")
-                       ("melpa" . "https://melpa.org/packages/")
-                       ("gnu"   . "https://elpa.gnu.org/packages/")))
-  (package-initialize)
-  (unless (package-installed-p 'leaf)
-    (package-refresh-contents)
-    (package-install 'leaf))
-
-  (leaf leaf-keywords
-    :ensure t
-    :init
-    ;; optional packages if you want to use :hydra, :el-get, :blackout,,,
-    (leaf hydra :ensure t)
-    (leaf el-get :ensure t)
-    (leaf blackout :ensure t)
-
-    :config
-    ;; initialize leaf-keywords.el
-    (leaf-keywords-init)))
-
-(leaf leaf
-  :config
-  (leaf leaf-convert :ensure t)
-  (leaf leaf-tree
-    :ensure t
-    :custom ((imenu-list-size . 30)
-             (imenu-list-position . 'left))))
-
-;; We do not use costom.el.
-(leaf cus-edit
-  :doc "tools for customizing Emacs and Lisp packages"
-  :tag "builtin" "faces" "help"
-  :custom `((custom-file . ,(locate-user-emacs-file "custom.el"))))
-
-;; You can also configure builtin package via leaf!
-(leaf cus-start
-  :doc "define customization properties of builtins"
-  :tag "builtin" "internal"
-  :bind (("\C-z" . undo))
-  :custom ((user-full-name . "Yoshihide Chubachi")
-           (user-mail-address . "yoshi@chubachi.net")
-           (user-login-name . "yc")
-           ;; (truncate-lines . t)
-           ;; (menu-bar-mode . t)
-           ;; (tool-bar-mode . nil)
-           ;; (scroll-bar-mode . nil)
-           ;; (indent-tabs-mode . nil)
-           (blink-cursor-mode . nil))
-  :config
-  (defalias 'yes-or-no-p 'y-or-n-p)
-  (keyboard-translate ?\C-h ?\C-?) ;; ???
-  )
-
-;; ;; builtin packages
-(leaf paren
-  :doc "highlight matching paren"
-  :tag "builtin"
-  :custom ((show-paren-delay . 0.1))
-  :global-minor-mode show-paren-mode)
-
-(leaf isearch
-  :bind ((isearch-mode-map
-	  ("C-o" . isearch-toggle-input-method))))
-
-;; 多分設定が中途はんぱ
-(leaf wdired
-  :doc "Rename files editing their names in dired buffers"
-  :tag "builtin"
-  :added "2020-11-21"
-  :bind ((wdired-mode-map
-          ("C-o" . toggle-input-method))))
-
-(leaf autorevert
-  :doc "revert buffers when files on disk change"
-  :tag "builtin"
-  :custom ((auto-revert-interval . 0.1))
-  :global-minor-mode global-auto-revert-mode)
-
-(leaf macrostep
-  :doc "interactive macro expander"
-  :req "cl-lib-0.5"
-  :tag "debugging" "macro" "languages" "lisp"
-  :added "2020-11-21"
-  :url "https://github.com/joddie/macrostep"
-  :bind (("C-c e" . macrostep-expand)))
-
-(leaf mozc-im
-  :doc "Mozc with input-method-function interface."
-  :req "mozc-0"
-  :tag "extentions" "i18n"
-  :added "2020-11-21"
-  :ensure t
-  :require mozc-im
-  :custom
-  ((default-input-method . "japanese-mozc-im"))
-  :bind (("C-o" . toggle-input-method))
-  )
-
-(leaf flycheck
-  :doc "On-the-fly syntax checking"
-  :emacs>= 24.3
-  :ensure t
-  :bind (("M-n" . flycheck-next-error)
-         ("M-p" . flycheck-previous-error))
-  :custom ((flycheck-emacs-lisp-initialize-packages . t))
-  :hook (emacs-lisp-mode-hook lisp-interaction-mode-hook)
-  :config
-  (leaf flycheck-package
-    :doc "A Flycheck checker for elisp package authors"
-    :ensure t
-    :config
-    (flycheck-package-setup))
-
-  (leaf flycheck-elsa
-    :doc "Flycheck for Elsa."
-    :emacs>= 25
-    :ensure t
-    :config
-    (flycheck-elsa-setup))
-  )
-
-(provide 'init)
+(require 'org-install)
+(org-babel-load-file "README.org")
 
 ;; Local Variables:
 ;; indent-tabs-mode: nil
 ;; End:
-
 ;;; init.el ends here
