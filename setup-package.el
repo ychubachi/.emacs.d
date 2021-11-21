d3596258b753988f6a18c70d856341f1e0d1ade0
Yoshihide Chubachi <yoshi@chubachi.net>
2021-11-22 00:24:16 +0900

    orgのリポジトリを削除

--- setup-package.el
+++ setup-package.el
@@ -2,7 +2,7 @@
 
 (prog1 "package"
   (custom-set-variables
-   '(package-archives '(("org"   . "https://orgmode.org/elpa/")
+   '(package-archives '(;; ("org"   . "https://orgmode.org/elpa/")
                         ("melpa" . "https://melpa.org/packages/")
                         ("gnu"   . "https://elpa.gnu.org/packages/"))))
   (package-initialize))
