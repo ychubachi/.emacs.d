4a3870c81e93e03b1a4004e4b601202cb3f6b6f7
Yoshihide Chubachi <yoshi@chubachi.net>
2021-11-26 14:52:03 +0900

    setup-package.elにorgのリポジトリを戻した

--- setup-package.el
+++ setup-package.el
@@ -1,8 +1,8 @@
-;; emacs -l ~/.emacs.d/setup-package.el
+;; emacs -q -l ~/.emacs.d/setup-package.el
 
 (prog1 "package"
   (custom-set-variables
-   '(package-archives '(;; ("org"   . "https://orgmode.org/elpa/")
+   '(package-archives '(("org"   . "https://orgmode.org/elpa/")
                         ("melpa" . "https://melpa.org/packages/")
                         ("gnu"   . "https://elpa.gnu.org/packages/"))))
   (package-initialize))
