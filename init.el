bf1bfd601e1928e51f31b0a6dfb50d629576e0dd
Yoshihide Chubachi <yoshi@chubachi.net>
2023-02-10 02:47:09 +0900

    Update

--- init.el
+++ init.el
@@ -52,7 +52,7 @@
     (package-refresh-contents)
     (package-install 'leaf)))
 
-(leaf *leaf
+(leaf *Leaf-Related-Packages
   :config
   (leaf leaf-keywords
     :ensure t
@@ -71,9 +71,9 @@
     ;;          (imenu-list-position . 'left))
     )
 
-  (leaf leaf-convert :ensure t))
+  (leaf leaf-convert :ensure t)
 
-(leaf *straight.el
+  (leaf *straight.el
   :init
   (defvar bootstrap-version)
   (let ((bootstrap-file
@@ -86,38 +86,50 @@
            'silent 'inhibit-cookies)
         (goto-char (point-max))
         (eval-print-last-sexp)))
-    (load bootstrap-file nil 'nomessage)))
+    (load bootstrap-file nil 'nomessage))))
 
-;;;; Hi-priority packages
-(leaf no-littering
-  :url "https://github.com/emacscollective/no-littering#usage"
-  :straight t :require t)
-
-(leaf org :straight t)
-
-(leaf macrostep ; to test leaf macros.
-  :doc "interactive macro expander"
-  :req "cl-lib-0.5"
-  :tag "debugging" "macro" "languages" "lisp"
-  :url "https://github.com/joddie/macrostep"
-  :straight t
-  :bind (("C-c e" . macrostep-expand)))
-;;;; Packages
-
-(leaf *lang-env
+(leaf *Hi-Priority-Packages
   :init
-  (set-language-environment "Japanese")
-  (prefer-coding-system 'utf-8)
-  (cond ((eq system-type 'windows-nt)
-         (setq default-process-coding-system (cons 'utf-8 'cp932-unix)))))
+  (leaf no-littering
+    :url "https://github.com/emacscollective/no-littering#usage"
+    :straight t :require t)
 
-(leaf *keyboard
+  (leaf org :straight t)
+
+  (leaf macrostep ; to test leaf macros.
+    :doc "interactive macro expander"
+    :url "https://github.com/joddie/macrostep"
+    :straight t
+    :bind (("C-c e" . macrostep-expand))))
+
+(leaf *Keyboard-Sttings
   :init
-  (define-key key-translation-map [?\C-h] [?\C-?])
-  (global-set-key (kbd "C-^") help-map)
+  (leaf *Help-Keys
+    :init
+    (define-key key-translation-map [?\C-h] [?\C-?])
+    (global-set-key (kbd "C-^") help-map))
+
   (leaf *undo :bind (("C-z" . undo))))
 
-(leaf mozc
+(leaf *Japanese-Related-Settings
+  :init
+  (leaf *Language-Environment
+    :init
+    (set-language-environment "Japanese")
+    (prefer-coding-system 'utf-8)
+    (cond ((eq system-type 'windows-nt)
+           (setq default-process-coding-system (cons 'utf-8 'cp932-unix)))))
+
+  (leaf *Windows-Fonts
+    :disabled t
+    :if (eq system-type 'windows-nt)
+    :init
+    ;; 通常使用するフォント
+    (set-frame-font "PlemolJP-12" nil t)
+    ;; IME未確定時のフォント設定
+    (modify-all-frames-parameters '((ime-font . "PlemolJP-12"))))
+
+  (leaf mozc
   :straight t
   :config
   (cond ((eq system-type 'windows-nt)
@@ -162,29 +174,101 @@
     :config
     (advice-add 'mozc-session-execute-command
   	        :after (lambda (&rest args)
-  		         (when (eq (nth 0 args) 'CreateSession)
-  		           (mozc-session-sendkey '(Hankaku/Zenkaku)))))))
+  	                 (when (eq (nth 0 args) 'CreateSession)
+  	                   (mozc-session-sendkey '(Hankaku/Zenkaku)))))))
 
-(leaf *fonts-win
-  :if (eq system-type 'windows-nt)
-  :init
-  ;; 通常使用するフォント
-  (set-frame-font "PlemolJP-12" nil t)
-  ;; 行間
-  (setq-default line-spacing 0)
-  ;; IME未確定時のフォント設定
-  (modify-all-frames-parameters '((ime-font . "PlemolJP-12")))
   )
 
-(leaf goto-addr.el
-  :doc "Toggle Goto-Address mode in all buffers."
-  :url "https://www.gnu.org/software/emacs/manual/html_node/emacs/Goto-Address-mode.html"
+(leaf *Look-And-Feel
+  :custom
+  (line-spacing . 0.2)
   :init
-  ;; You can follow the URL by typing C-c RET
-  (global-goto-address-mode 1))
+  (leaf modus-themes
+    :straight t                        ; omit this to use the built-in themes
+    :custom
+    (modus-themes-italic-constructs . t)
+    (modus-themes-bold-constructs . nil)
+    (modus-themes-region . '(bg-only no-extend))
+    (modus-themes-org-blocks . 'gray-background) ; {nil,'gray-background,'tinted-background}
+    (modus-themes-subtle-line-numbers . t)
+    (modus-themes-mode-line . '(moody borderless (padding . 0) (height . 0.9)))
+    (modus-themes-syntax . '(yellow-comments green-strings))
+    ;; (modus-themes-hl-line . '(underline accented)) ;'(underline accented)
+    (modus-themes-paren-match . '(intense underline))
+    (modus-themes-headings ; this is an alist: read the manual or its doc string
+     . '((1 . (regular 1.215))
+         (2 . (regular 1.138))
+         (3 . (regular 1.067))
+         (t . (regular))))
+    :init
+    (require-theme 'modus-themes)
+    ;; Load the theme of your choice:
+    ;; (load-theme 'modus-operandi :no-confirm)
+    (load-theme 'modus-vivendi :no-confirm)
+    :bind
+    ("<f5>" . modus-themes-toggle))
+
+    (leaf org-modern
+    :disabled nil
+    :url "https://github.com/minad/org-modern"
+    :straight t
+    :init
+    ;; Add frame borders and window dividers
+    (modify-all-frames-parameters
+     '((right-divider-width . 10)
+       (internal-border-width . 10)))
+    (dolist (face '(window-divider
+                    window-divider-first-pixel
+                    window-divider-last-pixel))
+      (face-spec-reset-face face)
+      (set-face-foreground face (face-attribute 'default :background)))
+    (set-face-background 'fringe (face-attribute 'default :background))
+
+    ;; (setq org-modern-star '("🟩" "🟣" "🔶" "◎" "○" "※"))
+    ;; (setq org-modern-star '("■" "◆" "◎" "○" "§" "¶"))
+    ;; (setq org-modern-star '("🟧" "🔶" "🟠" "🔸" "§" "¶"))
+    (setq org-modern-star '("■" "◆" "●" "◎" "○" "・"))
+
+    (setq
+     ;; ;; Edit settings
+     org-auto-align-tags nil ; Non-nil keeps tags aligned when modifying headlines.
+     ;; org-tags-column 0
+     ;; org-catch-invisible-edits 'show-and-error
+     ;; org-special-ctrl-a/e t
+
+     ;; ;; Org styling, hide markup etc.
+     org-hide-emphasis-markers t
+     ;; org-pretty-entities t
+     ;; org-ellipsis "…"
+
+     ;; ;; Agenda styling
+     ;; org-agenda-tags-column 0
+     ;; org-agenda-block-separator ?─
+     ;; org-agenda-time-grid
+     ;; '((daily today require-timed)
+     ;;   (800 1000 1200 1400 1600 1800 2000)
+     ;;   " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
+     ;; org-agenda-current-time-string
+     ;; "⭠ now ─────────────────────────────────────────────────"
+     )
+    (global-org-modern-mode)))
+
+(leaf *Global-Minnor-Mode
+  :init
+  (leaf goto-addr.el
+    :doc "Toggle Goto-Address mode in all buffers."
+    :url "https://www.gnu.org/software/emacs/manual/html_node/emacs/Goto-Address-mode.html"
+    :init
+    ;; You can follow the URL by typing C-c RET
+    (global-goto-address-mode 1)))
 
 ;; Load my settings
-(leaf *my-settings :disabled nil
+(leaf *Test-Bed
+  :init
+  ;; Do something
+  )
+
+(leaf *Load-My-Settings :disabled nil
   :init
   (my-tick-init-time "loading README.org")
   (org-babel-load-file "~/.emacs.d/README.org"))
