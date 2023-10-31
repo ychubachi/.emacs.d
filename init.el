4289a7d9af434db9b504c8a8487b9d7ee53a97a3
Yoshihide Chubachi <yoshi@chubachi.net>
2023-10-31 21:53:00 +0900

    corfu

--- init.el
+++ init.el
@@ -1658,48 +1658,8 @@ _~_: modified
 
       (leaf After-Init-Hook
         :init
-        (leaf corfu
-          :doc "Completion Overlay Region FUnction"
-          :url "https://github.com/minad/corfu"
-          :straight t
-          ;; Optional customizations
-          :custom
-          (corfu-cycle . t) ;; Enable cycling for `corfu-next/previous'
-          (corfu-auto . t)  ;; Enable auto completion
-          ;; (corfu-separator ?\s)          ;; Orderless field separator
-          ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
-          ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-          ;; (corfu-preview-current nil)    ;; Disable current candidate preview
-          ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
-          ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
-          ;; (corfu-scroll-margin 5)        ;; Use scroll margin
-
-          ;; Enable Corfu only for certain modes.
-          ;; :hook ((prog-mode . corfu-mode)
-          ;;        (shell-mode . corfu-mode)
-          ;;        (eshell-mode . corfu-mode))
-
-          ;; Recommended: Enable Corfu globally.
-          ;; This is recommended since Dabbrev can be used globally (M-/).
-          ;; See also `corfu-excluded-modes'.
-          :init
-          (global-corfu-mode))
-
-        ;; A few more useful configurations...
-        (leaf TODO:emacs
-          :init
-          ;; TAB cycle if there are only few candidates
-          (setq completion-cycle-threshold 3)
-
-          ;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
-          ;; Corfu commands are hidden, since they are not supposed to be used via M-x.
-          ;;
-          (setq read-extended-command-predicate
-                #'command-completion-default-include-p)
-
-          ;; Enable indentation+completion using the TAB key.
-          ;; `completion-at-point' is often bound to M-TAB.
-          (setq tab-always-indent 'complete))))
+        )
+      )
     )
 
   (leaf Original-Packages
@@ -1757,6 +1717,50 @@ _~_: modified
   (leaf origami
     :url "http://emacs.rubikitch.com/origami/"
     :straight t)
+
+  (leaf corfu
+    :disabled t ;; TODO
+    :doc "Completion Overlay Region FUnction"
+    :url "https://github.com/minad/corfu"
+    :straight t
+    ;; Optional customizations
+    ;; :custom
+    ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+    ;; (corfu-auto t)                 ;; Enable auto completion
+    ;; (corfu-separator ?\s)          ;; Orderless field separator
+    ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+    ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+    ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+    ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+    ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+    ;; (corfu-scroll-margin 5)        ;; Use scroll margin
+
+    ;; Enable Corfu only for certain modes.
+    ;; :hook ((prog-mode . corfu-mode)
+    ;;        (shell-mode . corfu-mode)
+    ;;        (eshell-mode . corfu-mode))
+
+    ;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
+    ;; be used globally (M-/).  See also the customization variable
+    ;; `global-corfu-modes' to exclude certain modes.
+    :init
+    (global-corfu-mode)
+
+    ;; A few more useful configurations...
+    (leaf emacs
+      :init
+      ;; TAB cycle if there are only few candidates
+      (setq completion-cycle-threshold 3)
+
+      ;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
+      ;; Corfu commands are hidden, since they are not supposed to be used via M-x.
+      ;;
+      (setq read-extended-command-predicate
+            #'command-completion-default-include-p)
+
+      ;; Enable indentation+completion using the TAB key.
+      ;; `completion-at-point' is often bound to M-TAB.
+      (setq tab-always-indent 'complete)))
   )
 
 (leaf Test-Bed
