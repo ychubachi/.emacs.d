843f65ec980d54e8d197e9d1976831b9d87a700d
Yoshihide Chubachi <yoshi@chubachi.net>
2025-01-06 21:15:45 +0900

    init.elのパッケージ初期化をearly-init.elに移動

--- early-init.el
+++ early-init.el
@@ -1,7 +1,60 @@
 ;;; early-init.el --- My early-init.el  -*- lexical-binding: t; -*-
-;; https://github.com/emacscollective/no-littering/blob/master/README.org
+;; Copyright (C) 2022-2025 Yoshihide Chubachi
+
+;; Author: Yoshihide Chubachi <yoshi@chubachi.net>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; このファイルはearly-init.orgから生成します。
+
+;;; Code:
+
+(eval-and-compile
+  (prog1 "package"
+    (customize-set-variable
+     'package-archives '(("org" . "https://orgmode.org/elpa/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("gnu" . "https://elpa.gnu.org/packages/")))
+    (package-initialize))
+
+  (prog1 "straight.el"
+    (defvar bootstrap-version)
+    (let ((bootstrap-file
+           (expand-file-name
+            "straight/repos/straight.el/bootstrap.el"
+            (or (bound-and-true-p straight-base-dir)
+                user-emacs-directory)))
+          (bootstrap-version 7))
+      (unless (file-exists-p bootstrap-file)
+        (with-current-buffer
+            (url-retrieve-synchronously
+             "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+             'silent 'inhibit-cookies)
+          (goto-char (point-max))
+          (eval-print-last-sexp)))
+      (load bootstrap-file nil 'nomessage)))
+
+  (prog1 "leaf"
+    (straight-use-package 'leaf)
+    (straight-use-package 'leaf-keywords)
+    (leaf-keywords-init)))
+
 (when (fboundp 'startup-redirect-eln-cache)
   (startup-redirect-eln-cache
    (convert-standard-filename
-	  (expand-file-name  "var/eln-cache/" user-emacs-directory))))
+          (expand-file-name  "var/eln-cache/" user-emacs-directory))))
+
 ;;; early-init.el ends here
--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 ;;; init.el --- My init.el  -*- lexical-binding: t; -*-
-;; Copyright (C) 2022-2024 Yoshihide Chubachi
+;; Copyright (C) 2022-2025 Yoshihide Chubachi
 
 ;; Author: Yoshihide Chubachi <yoshi@chubachi.net>
 
@@ -18,39 +18,9 @@
 
 ;;; Commentary:
 
-;; このファイルはinit.orgから生成します。
+;; このファイルはinit.orgから手動生成する。先にearly-init.elが読まれる。
 
 ;;; Code:
-
-(eval-and-compile
-  (prog1 "package"
-    (customize-set-variable
-     'package-archives '(("org" . "https://orgmode.org/elpa/")
-                         ("melpa" . "https://melpa.org/packages/")
-                         ("gnu" . "https://elpa.gnu.org/packages/")))
-    (package-initialize))
-
-  (prog1 "straight.el"
-    (defvar bootstrap-version)
-    (let ((bootstrap-file
-           (expand-file-name
-            "straight/repos/straight.el/bootstrap.el"
-            (or (bound-and-true-p straight-base-dir)
-                user-emacs-directory)))
-          (bootstrap-version 7))
-      (unless (file-exists-p bootstrap-file)
-        (with-current-buffer
-            (url-retrieve-synchronously
-             "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-             'silent 'inhibit-cookies)
-          (goto-char (point-max))
-          (eval-print-last-sexp)))
-      (load bootstrap-file nil 'nomessage)))
-
-  (prog1 "leaf"
-    (straight-use-package 'leaf)
-    (straight-use-package 'leaf-keywords)
-    (leaf-keywords-init)))
 
 (leaf org :straight t)
 
