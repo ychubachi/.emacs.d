e6c20e0db2327ba8de7651fe27116cf18ed4be11
Yoshihide CHUBACHI <yoshi@chubachi.net>
2026-07-01 21:47:36 +0900

    early-init.elから不要な設定を移しました

--- early-init.el
+++ early-init.el
@@ -18,71 +18,17 @@
 
 ;;; Commentary:
 
-;; このファイルはearly-init.orgから生成します。
+;; このファイルはearly-init.orgから手動で生成します（C-c C-v C-t）。
 
 ;;; Code:
 
+;; 起動時にガベージコレクションのしきい値を大きくして高速化
+(setq gc-cons-threshold 100000000)
+
 (setq package-enable-at-startup nil)
 
-(eval-and-compile
-  (prog1 "package"
-    (customize-set-variable
-     'package-archives '(("org" . "https://orgmode.org/elpa/")
-                         ("melpa" . "https://melpa.org/packages/")
-                         ("gnu" . "https://elpa.gnu.org/packages/")))
-    (package-initialize))
-
-  (prog1 "straight.el"
-    (defvar bootstrap-version)
-    (let ((bootstrap-file
-           (expand-file-name
-            "straight/repos/straight.el/bootstrap.el"
-            (or (bound-and-true-p straight-base-dir)
-                user-emacs-directory)))
-          (bootstrap-version 7))
-      (unless (file-exists-p bootstrap-file)
-        (with-current-buffer
-            (url-retrieve-synchronously
-             "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-             'silent 'inhibit-cookies)
-          (goto-char (point-max))
-          (eval-print-last-sexp)))
-      (load bootstrap-file nil 'nomessage)))
-
-  (prog1 "leaf"
-    (straight-use-package 'leaf)
-    (straight-use-package 'leaf-keywords)
-    (leaf-keywords-init)))
-
-(leaf leaf-convert :straight t)
-
-(leaf org :straight t)
-
-(leaf no-littering :straight t :require t
-        :url "https://github.com/emacscollective/no-littering#usage"
-        :init
-        (let ((dir (no-littering-expand-var-file-name "lock-files/")))
-          (make-directory dir t)
-          (setq lock-file-name-transforms `((".*" ,dir t))))
-
-        (require 'recentf)
-        (add-to-list 'recentf-exclude
-                     (recentf-expand-file-name no-littering-var-directory))
-        (add-to-list 'recentf-exclude
-                     (recentf-expand-file-name no-littering-etc-directory))
-
-        (custom-set-variables '(custom-file
-                                (no-littering-expand-etc-file-name "custom.el")))
-
-        (no-littering-theme-backups))
-
-(when (and (fboundp 'startup-redirect-eln-cache)
-         (fboundp 'native-comp-available-p)
-         (native-comp-available-p))
-(startup-redirect-eln-cache
- (convert-standard-filename
-  (expand-file-name  "var/eln-cache/" user-emacs-directory))))
-
-(load-theme 'misterioso)
+;; 起動時の画面の白飛びを防ぐ
+(add-to-list 'initial-frame-alist '(background-color . "#000000"))
+(add-to-list 'default-frame-alist '(background-color . "#000000"))
 
 ;;; early-init.el ends here
--- init.el
+++ init.el
@@ -22,6 +22,69 @@
 
 ;;; Code:
 
+(eval-and-compile
+  ;; (prog1 "package"
+  ;;   (customize-set-variable
+  ;;    'package-archives '(("org" . "https://orgmode.org/elpa/")
+  ;;                        ("melpa" . "https://melpa.org/packages/")
+  ;;                        ("gnu" . "https://elpa.gnu.org/packages/")))
+  ;;   (package-initialize))
+
+  (prog1 "straight.el"
+    (defvar straight-repository-branch "master")
+    (defvar straight-check-for-modifications '(find-when-checking))
+    (defvar bootstrap-version)
+    (let ((bootstrap-file
+           (expand-file-name
+            "straight/repos/straight.el/bootstrap.el"
+            (or (bound-and-true-p straight-base-dir)
+                user-emacs-directory)))
+          (bootstrap-version 7))
+      (unless (file-exists-p bootstrap-file)
+        (with-current-buffer
+            (url-retrieve-synchronously
+             "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+             'silent 'inhibit-cookies)
+          (goto-char (point-max))
+          (eval-print-last-sexp)))
+      (load bootstrap-file nil 'nomessage)))
+
+  (prog1 "leaf"
+    (straight-use-package 'leaf)
+    (straight-use-package 'leaf-keywords)
+    (leaf-keywords-init)))
+
+(leaf leaf-convert :straight t)
+
+(leaf org :straight t)
+
+(leaf no-littering :straight t :require t
+        :url "https://github.com/emacscollective/no-littering#usage"
+        :init
+        (let ((dir (no-littering-expand-var-file-name "lock-files/")))
+          (make-directory dir t)
+          (setq lock-file-name-transforms `((".*" ,dir t))))
+
+        (require 'recentf)
+        (add-to-list 'recentf-exclude
+                     (recentf-expand-file-name no-littering-var-directory))
+        (add-to-list 'recentf-exclude
+                     (recentf-expand-file-name no-littering-etc-directory))
+
+        (custom-set-variables '(custom-file
+                                (no-littering-expand-etc-file-name "custom.el")))
+
+        (no-littering-theme-backups))
+
+(when (and (fboundp 'startup-redirect-eln-cache)
+         (fboundp 'native-comp-available-p)
+         (native-comp-available-p))
+(startup-redirect-eln-cache
+ (convert-standard-filename
+  (expand-file-name  "var/eln-cache/" user-emacs-directory))))
+
+(load-theme 'misterioso)
+
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 
 (provide 'init.el)
