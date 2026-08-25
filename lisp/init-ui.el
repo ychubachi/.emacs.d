fba5667f466346cb1d95af0d39f4b1af6a2b553c
Yoshihide CHUBACHI <yoshi@chubachi.net>
2026-08-25 17:29:59 +0900

    以後ゆっくりとinit.old.orgの内容を移し替えていく

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -1,4 +1,16 @@
 ;;; テーマの設定
 (load-theme 'misterioso)
 
+;;; dashboard - Emacs起動時にダッシュボードを表示する
+
+(use-package dashboard
+  :config
+  ;; スタートアップフックに登録して起動時に表示
+  (dashboard-setup-startup-hook)
+
+  ;; ダッシュボードに表示する項目とその件数
+  (setq dashboard-items '((recents  . 5)   ; 最近開いたファイル
+                          (bookmarks . 5)  ; ブックマーク
+                          (projects . 5)   ; 最近のプロジェクト（Project.elやProjectile連携）
+                          (agenda . 5))))   ; Org-modeのアジェンダ（予定）
 (provide 'init-ui)
