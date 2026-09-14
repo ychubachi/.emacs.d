6a07249575774b17db9934685bd5c3b2c0b1ac2a
Yoshihide Chubachi <yoshi@chubachi.net>
2026-09-14 20:01:01 +0900

    indent

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -56,116 +56,116 @@
   :after org
   ;;recommended: defer until calling doct
                                         ;:commands (doct)
-          :config
-          (setq org-capture-templates
-                (doct '(("Memo" :keys "m"
-                         :file "~/Dropbox/Org/Memo.org"
-                         ; :empty-lines-before 1
-                         :empty-lines-after 1
-                         :datetree t
-                         :unnarrowed nil       ; t
-                         :jump-to-captured nil ;
-                         :template ("* %?"
-                                    ":PROPERTIES:"
-                                    ":CREATED: %U"
-                                    ":LINK: %a"
-                                    ":END:"))
-                        ("Memo (unarrowed)" :keys "M"
-                         :file "~/Dropbox/Org/Memo.org"
-                         ; :empty-lines-before 1
-                         :empty-lines-after 1
-                         :datetree t
-                         :unnarrowed t
-                         :jump-to-captured t
-                         :template ("* %?"
-                                    ":PROPERTIES:"
-                                    ":CREATED: %U"
-                                    ":LINK: %a"
-                                    ":END:"))
-                        ("Todo" :keys "t"
-                         :file "~/Dropbox/Org/Memo.org"
-                         :datetree t
-                         :empty-lines-before 1
-                         :template ("* TODO %?"
-                                    ":PROPERTIES:"
-                                    ":CREATED: %U"
-                                    ":LINK: %a"
-                                    ":END:"))
-                        ("Notebook" :keys "n"
-                         :prepend t
-                         :empty-lines-after 1
-                         :file "~/Dropbox/Org/Notebook.org"
-                         :unnarrowed t
-                         :template ("* %^{Description}"
-                                    ":PROPERTIES:"
-                                    ":CREATED: %T"
-                                    ":END:"
-                                    "\n%?"))
-                        ("Post" :keys "p"
-                         :file "~/Dropbox/Org/Memo.org"
-                         :datetree t
-                         :unnarrowed t
-                         :jump-to-captured nil
-                         :empty-lines-before 1
-                                          ; :empty-lines-after 1
-                         :todo-state "TODO"
-                         :export_file_name (lambda () (concat (format-time-string "%Y-%m-%d-%H-%M-%S") ".html"))
-                         :template ("* %{todo-state} %^{Headline} :POST:"
-                                    ":PROPERTIES:"
-                                    ":CREATED: %U"
-                                    ":EXPORT_FILE_NAME: ~/git/ploversky-jekyll/_drafts/drafts_%{export_file_name}"
-                                    ":EXPORT_OPTIONS: toc:nil num:nil html5-fancy:t"
-                                    ":EXPORT_HTML_DOCTYPE: html5"
-                                    ":DIR: ~/git/ploversky-jekyll/assets/images/posts/"
-                                    ":END:"
-                                    ""
-                                    "#+begin_comment"
-                                    "First time: C-c C-e C-b C-s h h (Do this here)"
-                                    "Next  time: C-u C-c C-e         (Do this anyware in the subtree)"
-                                    "#+end_comment"
-                                    ""
-                                    "#+begin_export html"
-                                    "---"
-                                    "layout: post"
-                                    "title:"
-                                    "categories:"
-                                    "tags:"
-                                    "published: true"
-                                    "---"
-                                    "#+end_export"
-                                    "\n**  %?"))
-                        ("Blog" :keys "b"
-                         :prepend t
-                         :empty-lines-after 1
-                         :unnarrowed t
-                         :children
-                         (("ploversky@zenn.dev" :keys "z"
-                           :file "~/git/ploversky-zenn.dev/plaversky@zenn.dev.org"
-                           :headline   "記事"
-                           :todo-state "TODO"
-                           :export_file_name (lambda () (concat (format-time-string "%Y%m%d-%H%M%S")))
-                           :template ("* %{todo-state} %^{Description}"
-                                      ":PROPERTIES:"
-                                      ":CREATED: %T"
-                                      ":EXPORT_FILE_NAME: articles/%{export_file_name}"
-                                      ":EXPORT_GFM_TAGS: blog"
-                                      ":EXPORT_GFM_CUSTOM_FRONT_MATTER: :emoji 👩‍💻"
-                                      ":EXPORT_GFM_CUSTOM_FRONT_MATTER+: :type tech"
-                                      ":EXPORT_GFM_CUSTOM_FRONT_MATTER+: :published false"
-                                      ":END:"
-                                      "\n* %?"))
-                          ("blog.chubachi.net"  :keys "b"
-                           :file "~/git/ychubachi.github.io/blog.chubachi.net.org"
-                           :headline   "Blog"
-                           :todo-state "TODO"
-                           :export_file_name (lambda () (concat (format-time-string "%Y%m%d-%H%M%S")))
-                           :template ("* %{todo-state} %^{Description}"
-                                      ":PROPERTIES:"
-                                      ":CREATED: %T"
-                                      ":EXPORT_FILE_NAME: %{export_file_name}"
-                                      ":EXPORT_DATE: %U"
-                                      ":END:"
-                                      "\n** %?"))))))))
+  :config
+  (setq org-capture-templates
+        (doct '(("Memo" :keys "m"
+                 :file "~/Dropbox/Org/Memo.org"
+                                        ; :empty-lines-before 1
+                 :empty-lines-after 1
+                 :datetree t
+                 :unnarrowed nil       ; t
+                 :jump-to-captured nil ;
+                 :template ("* %?"
+                            ":PROPERTIES:"
+                            ":CREATED: %U"
+                            ":LINK: %a"
+                            ":END:"))
+                ("Memo (unarrowed)" :keys "M"
+                 :file "~/Dropbox/Org/Memo.org"
+                                        ; :empty-lines-before 1
+                 :empty-lines-after 1
+                 :datetree t
+                 :unnarrowed t
+                 :jump-to-captured t
+                 :template ("* %?"
+                            ":PROPERTIES:"
+                            ":CREATED: %U"
+                            ":LINK: %a"
+                            ":END:"))
+                ("Todo" :keys "t"
+                 :file "~/Dropbox/Org/Memo.org"
+                 :datetree t
+                 :empty-lines-before 1
+                 :template ("* TODO %?"
+                            ":PROPERTIES:"
+                            ":CREATED: %U"
+                            ":LINK: %a"
+                            ":END:"))
+                ("Notebook" :keys "n"
+                 :prepend t
+                 :empty-lines-after 1
+                 :file "~/Dropbox/Org/Notebook.org"
+                 :unnarrowed t
+                 :template ("* %^{Description}"
+                            ":PROPERTIES:"
+                            ":CREATED: %T"
+                            ":END:"
+                            "\n%?"))
+                ("Post" :keys "p"
+                 :file "~/Dropbox/Org/Memo.org"
+                 :datetree t
+                 :unnarrowed t
+                 :jump-to-captured nil
+                 :empty-lines-before 1
+                                        ; :empty-lines-after 1
+                 :todo-state "TODO"
+                 :export_file_name (lambda () (concat (format-time-string "%Y-%m-%d-%H-%M-%S") ".html"))
+                 :template ("* %{todo-state} %^{Headline} :POST:"
+                            ":PROPERTIES:"
+                            ":CREATED: %U"
+                            ":EXPORT_FILE_NAME: ~/git/ploversky-jekyll/_drafts/drafts_%{export_file_name}"
+                            ":EXPORT_OPTIONS: toc:nil num:nil html5-fancy:t"
+                            ":EXPORT_HTML_DOCTYPE: html5"
+                            ":DIR: ~/git/ploversky-jekyll/assets/images/posts/"
+                            ":END:"
+                            ""
+                            "#+begin_comment"
+                            "First time: C-c C-e C-b C-s h h (Do this here)"
+                            "Next  time: C-u C-c C-e         (Do this anyware in the subtree)"
+                            "#+end_comment"
+                            ""
+                            "#+begin_export html"
+                            "---"
+                            "layout: post"
+                            "title:"
+                            "categories:"
+                            "tags:"
+                            "published: true"
+                            "---"
+                            "#+end_export"
+                            "\n**  %?"))
+                ("Blog" :keys "b"
+                 :prepend t
+                 :empty-lines-after 1
+                 :unnarrowed t
+                 :children
+                 (("ploversky@zenn.dev" :keys "z"
+                   :file "~/git/ploversky-zenn.dev/plaversky@zenn.dev.org"
+                   :headline   "記事"
+                   :todo-state "TODO"
+                   :export_file_name (lambda () (concat (format-time-string "%Y%m%d-%H%M%S")))
+                   :template ("* %{todo-state} %^{Description}"
+                              ":PROPERTIES:"
+                              ":CREATED: %T"
+                              ":EXPORT_FILE_NAME: articles/%{export_file_name}"
+                              ":EXPORT_GFM_TAGS: blog"
+                              ":EXPORT_GFM_CUSTOM_FRONT_MATTER: :emoji 👩‍💻"
+                              ":EXPORT_GFM_CUSTOM_FRONT_MATTER+: :type tech"
+                              ":EXPORT_GFM_CUSTOM_FRONT_MATTER+: :published false"
+                              ":END:"
+                              "\n* %?"))
+                  ("blog.chubachi.net"  :keys "b"
+                   :file "~/git/ychubachi.github.io/blog.chubachi.net.org"
+                   :headline   "Blog"
+                   :todo-state "TODO"
+                   :export_file_name (lambda () (concat (format-time-string "%Y%m%d-%H%M%S")))
+                   :template ("* %{todo-state} %^{Description}"
+                              ":PROPERTIES:"
+                              ":CREATED: %T"
+                              ":EXPORT_FILE_NAME: %{export_file_name}"
+                              ":EXPORT_DATE: %U"
+                              ":END:"
+                              "\n** %?"))))))))
 
 ;;; org-sidebar - Orgの構造をサイドバーに表示
 (use-package org-sidebar
@@ -173,7 +173,7 @@
   :custom
   (org-sidebar-tree-side 'left))
 
-;;; org-tempo - begin_...を簡単に
+;;; org-tempo - #begin_...を簡単に
 ;; <el TAB -> #begin_src elisp
 
 (use-package org-tempo
